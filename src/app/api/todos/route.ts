151d726498acb36774fda12ef257085597d3a132
SONGHEE <pasong0511@gmail.com>
2023-12-01 23:39:11 +0900

    feat: POST API 구현 & 클라이언트 구현

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -19,3 +19,27 @@ export async function GET(request: NextRequest) {
     );
   }
 }
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { desc } = reqBody;
+    console.log(desc);
+
+    const newTodo = new Todo({
+      id: v4(),
+      desc,
+      completed: false,
+    });
+
+    const savedTodo = await newTodo.save();
+    return NextResponse.json({ msg: "todo added", success: true, savedTodo });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        msg: "이슈 발생!",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,14 +1,42 @@
 "use client";
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface TodoProps {
+  id: string;
+  desc: string;
+  completed: boolean;
+}
 
 export default function Todos() {
-  const [inputText, setInputText] = useState();
-  const [todos, setTodos] = useState([1, 2, 3]);
+  const [inputText, setInputText] = useState<string>("");
+  const [todos, setTodos] = useState<TodoProps[]>([]);
   const [editMode, setEditMode] = useState(false);
 
   const onChange = (e: any) => {
     setInputText(e.target.value);
   };
+
+  const addTodos = async () => {
+    const data = {
+      desc: inputText,
+    };
+    const res = await axios.post("/api/todos", data);
+    console.log("res", res.data.savedTodo);
+    setTodos([...todos, res.data.savedTodo]);
+    clearTodos();
+  };
+
+  const clearTodos = () => {
+    setInputText("");
+  };
+
+  useEffect(() => {
+    axios.get("/api/todos").then((res) => {
+      console.log(res.data);
+      setTodos(res.data.todos);
+    });
+  }, []);
 
   if (editMode) {
     return (
@@ -44,7 +72,10 @@ export default function Todos() {
           value={inputText}
           onChange={onChange}
         />
-        <div className="text-xl shadow-md bg-blue-600 text-white hover:bg-blue-500 rounded-md px-3 py-1">
+        <div
+          onClick={addTodos}
+          className="text-xl shadow-md bg-blue-600 text-white hover:bg-blue-500 rounded-md px-3 py-1"
+        >
           Add
         </div>
         <div className="text-xl shadow-md bg-gray-600 text-white hover:bg-gray-500 rounded-md px-3 py-1">
@@ -54,10 +85,13 @@ export default function Todos() {
       <div className="w-5/6 flex flex-col gap-2">
         {todos.map((todo, index) => {
           return (
-            <div className="bg-violet-600 flex justify-between items-center items-center p-2 rounded-lg shadow-md">
+            <div
+              key={todo.id}
+              className="bg-violet-600 flex justify-between items-center items-center p-2 rounded-lg shadow-md"
+            >
               <div className="flex gap-2">
-                <input type="checkbox" />
-                <div className="text-lg text-white">코드입력</div>
+                <input type="checkbox" defaultChecked={todo.completed} />
+                <div className="text-lg text-white">{todo.desc}</div>
               </div>
               <div className="flex gap-2">
                 <button className="text-m shadow-md bg-green-600 text-white hover:bg-green-500 rounded-md px-1">
